90e53fe6ba4387dddb8a2300199e5c2f20e5c440
deepanshu <deepanshukamlesh@gamil.com>
2024-05-15 23:03:54 +0530

    Redirects the user to the original url using GET route

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ const app = express();
 
 const urlRoutes = require("./routes/url.js");
 
-const {connectToMongoDB} = require("./connect.js");
+const { connectToMongoDB } = require("./connect.js");
+
+const URL = require("./models/url.js");
 
 const PORT = 8001;
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url");
 
-app.use(express.json());  // this middleware will help in parsing the incoming client request body
+app.use(express.json()); // this middleware will help in parsing the incoming client request body
 
-app.use("/url",urlRoutes);
+app.use("/url", urlRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server started at PORT : ${PORT}`);
-})
+// Redirects the user to the original url
+app.get("/url/:shortId", async (req, res) => {
+  // dynamic route
+
+  const shortId = req.params.shortId;
+
+  // this will find the entry in database using shortId and will update visitHistory(it will push in array) , and in end will give us the document/entry/record corressponding to it
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamps: Date.now(),
+        },
+      },
+    }
+  );
+
+  res.redirect(entry.redirectUrl);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at PORT : ${PORT}`);
+});
